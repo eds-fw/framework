59576348f2d53c51ed3a0b5445dc2d96f251ff09
Kzzmz <demyantkachev2018@gmail.com>
2023-08-21 11:45:42 +0300

    1.1.3 changes

--- src/LazyConstructors/createBot.ts
+++ src/LazyConstructors/createBot.ts
@@ -13,13 +13,13 @@ type KnownRuntimeProperties = {
 
 export function createBot(config: eds.ConfigExemplar): KnownRuntimeProperties
 {
-    runtimeStorage.setProp("config",                 config);
-    runtimeStorage.setProp("logger",                 new eds.Logger(config.logsPath, config.timeOffset));
-    runtimeStorage.setProp("client",                 new eds.Client(config));
-    runtimeStorage.setProp("componentManager",       new eds.ComponentManager);
-    runtimeStorage.setProp("slashCommandsManager",   new eds.SlashCommandsManager);
-    runtimeStorage.setProp("loader",                 new eds.Loader(config.commandsPath, false, config.doNotLoadFilesStartsWith, config.includeBuiltinCommands));
-    runtimeStorage.setProp("contextFactory",         new eds.ContextFactory);
-    runtimeStorage.setProp("handler",                new eds.Handler);
+    runtimeStorage.setProp("config",                config);
+    runtimeStorage.setProp("logger",                new eds.Logger(config.logsPath, config.timeOffset));
+    runtimeStorage.setProp("client",                new eds.Client(config));
+    runtimeStorage.setProp("componentManager",      new eds.ComponentManager);
+    runtimeStorage.setProp("slashCommandsManager",  new eds.SlashCommandsManager);
+    runtimeStorage.setProp("loader",                new eds.Loader(config.commandsPath, false, config.doNotLoadFilesStartsWith, config.includeBuiltinCommands));
+    runtimeStorage.setProp("contextFactory",        new eds.ContextFactory);
+    runtimeStorage.setProp("handler",               new eds.Handler);
     return runtimeStorage.get<KnownRuntimeProperties>("config", "logger", "client", "componentManager", "loader", "contextFactory", "handler", "slashCommandsManager");
 }
--- src/LazyConstructors/createDB.ts
+++ src/LazyConstructors/createDB.ts
@@ -2,7 +2,13 @@ import { eds, runtimeStorage } from "..";
 
 export function createDB<T extends eds.JSONSupportedDataTypes = eds.JSONSupportedDataTypes>(name: string, path: string, autosave?: boolean | number)
 {
-    const databases = runtimeStorage.getProp<Record<string, eds.Database>>("databases");
+    let databases = runtimeStorage.databases as Record<string, eds.Database>;
+    if (databases === undefined)
+    {
+        runtimeStorage.setProp("databases", {});
+        createDB<T>(name, path, autosave);
+        return;
+    }
     databases[name] = new eds.Database<T>(path, autosave);
     runtimeStorage.setProp("databases", databases);
 }
