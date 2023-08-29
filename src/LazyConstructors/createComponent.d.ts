6f1f894347b26b89a835bd86fbcea841aa4f4ca9
Kzzmz <demyantkachev2018@gmail.com>
2023-08-29 21:03:02 +0300

    fix: broken types at Lazy/createComponent

--- src/LazyConstructors/createComponent.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ComponentManager } from "..";
-export declare const createButton: ComponentManager["createButton"];
-export declare const createMenu: ComponentManager["createMenu"];
-export declare const createModal: ComponentManager["createModal"];
--- src/LazyConstructors/createComponent.js
+++ src/LazyConstructors/createComponent.js
@@ -1,4 +1,10 @@
-const { runtimeStorage } = require("..");
+const { runtimeStorage, ComponentManager } = require("..");
+
+/** @type {ComponentManager["createButton"]} */
 exports.createButton = (...params) => runtimeStorage.getProp("componentManager").createButton(...params);
+
+/** @type {ComponentManager["createMenu"]} */
 exports.createMenu = (...params) => runtimeStorage.getProp("componentManager").createMenu(...params);
+
+/** @type {ComponentManager["createModal"]} */
 exports.createModal = (...params) => runtimeStorage.getProp("componentManager").createModal(...params);
