975bdc037ff026ab33f3b1dcc01c8ef29cbd9522
dtkdtk <demyantkachev2018@gmail.com>
2024-05-26 22:00:00 +0300

    WIP: context menu commands

--- src/Types/SupportedInteractions.ts
+++ src/Types/SupportedInteractions.ts
@@ -2,14 +2,20 @@ import type {
     AnySelectMenuInteraction,
     ButtonInteraction,
     ChatInputCommandInteraction,
+    MessageContextMenuCommandInteraction,
     ModalMessageModalSubmitInteraction,
-    ModalSubmitInteraction
+    ModalSubmitInteraction,
+    UserContextMenuCommandInteraction
 } from "discord.js";
 
 export type SupportedInteractions =
     | ChatInputCommandInteraction
+    | ModalSubmitInteraction
+    | UserContextMenuCommandInteraction
+    | MessageComponentInteraction
+
+export type MessageComponentInteraction =
     | ButtonInteraction
     | AnySelectMenuInteraction
-    | ModalSubmitInteraction
     | ModalMessageModalSubmitInteraction
-//
+    | MessageContextMenuCommandInteraction
