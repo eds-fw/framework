ba9dbe8d71dbd8bd5427d30bed04e4abcf1ef058
dtkdtk <demyantkachev2018@gmail.com>
2024-05-26 22:02:41 +0300

    updated Command/* types

---
+++ src/Types/Command.ts
@@ -0,0 +1,79 @@
+import type { ChatInputCommandInteraction, Message, MessageContextMenuCommandInteraction, UserContextMenuCommandInteraction } from "discord.js";
+import type { eds, runtimeStorage } from "..";
+import type { SupportedInteractions } from "./SupportedInteractions"
+
+export type ContextType = "text" | "slash" | "interaction"// | "message-context-menu" | "user-context-menu";
+export type CommandType = Exclude<ContextType, "interaction">;
+
+export interface InteractionContext
+    <InteractionType extends SupportedInteractions = SupportedInteractions, CtxType extends ContextType = "interaction"> extends _BaseContext
+{
+    interaction: InteractionType;
+    __contextType: CtxType;
+}
+export interface TextCommandContext extends _BaseContext
+{
+    message: Message;
+    args: string[];
+    __contextType: "text";
+}
+export type CommandContext<T extends CommandType> =
+    T extends "text"                    ? TextCommandContext
+    : T extends "slash"                 ? SlashCommandContext
+    : T extends "message-context-menu"  ? InteractionContext<MessageContextMenuCommandInteraction>
+    : T extends "user-context-menu"     ? InteractionContext<UserContextMenuCommandInteraction>
+    : never;
+export type SlashCommandContext = InteractionContext<ChatInputCommandInteraction, "slash">;
+export type AnyContext = InteractionContext | SlashCommandContext | TextCommandContext;
+
+
+
+
+
+export interface CommandFile<T extends CommandType>
+{
+    /** Disables this command loading */
+    pragmaSkip?: boolean;
+    /** Disables this command logging */
+    pragmaNoLog?: boolean;
+    /** Command code */
+    run: (ctx: CommandContext<T>) => any;
+    /** Command info */
+    info: CommandInfo<T>;
+}
+
+
+
+
+
+export interface CommandHelpInfo
+{
+    name:               string;
+    type:               CommandType;
+
+    usage?:             string;
+    usageDocs?:         string;
+    desc?:              string;
+    category?:          string;
+    allowInDM?:         boolean;
+    hidden?:            boolean;
+    /** Do not add 'everyone' role ID @functional may affect command execution */
+    allowedRoles?:      string[];
+    /** @functional may affect command execution */
+    noCheckAccess?:     boolean;
+}
+
+export interface CommandInfo<T extends CommandType> extends CommandHelpInfo
+{
+    aliases?:       T extends "text" ? string[] : never;
+    nonPrefixed?:   T extends "text" ? boolean  : never;
+}
+
+
+
+
+interface _BaseContext extends eds.EmbedTemplateMethods
+{
+    universal: SupportedInteractions | Message;
+    runtime: typeof runtimeStorage;
+}
--- src/Types/Command/CommandInfo.ts
+++
@@ -1,7 +0,0 @@
-import type { CommandHelpInfo } from "./CommandHelpInfo";
-
-export interface CommandInfo<T extends boolean> extends CommandHelpInfo
-{
-    aliases?:       T extends true ? never : string[];
-    nonPrefixed?:   T extends true ? never : boolean;
-}
